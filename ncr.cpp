2ccbf60b41a3dca8ed78ee514c6639d04b487480
rika0384 <wakabayashirika@rika-2.local>
2018-08-04 01:04:22 +0900

    add nCr

--- ncr.cpp
+++ ncr.cpp
@@ -45,7 +45,28 @@ void calc(){
     ncr[i][j] = ncr[i-1][j-1] + ncr[i-1][j];
   }
 }
+//-----------------------------
 
+long long fac[MAX], finv[MAX], inv[MAX];
+
+void init() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+long long ncr(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+//-----------------------------
 
 int main()
 {
@@ -55,4 +76,3 @@ int main()
   w--;
   cout<<ncr(h+w,h)<<endl;
 }
-
