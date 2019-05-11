c69cdc3e208648eb1e20b81355cf0bb4f81fbdeb
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-05-11 19:10:12 +0900

    fix SegTree

--- SegTree.cpp
+++ SegTree.cpp
@@ -16,42 +16,52 @@ public:
     SegTree (){}
 
     SegTree(int size){
-		n=1;
-		while(n<size)n<<=1;
-		seg=vector<int>(2*n,init);
+		n = 1;
+		while(n < size) n <<= 1;
+		seg = vector<int>(2*n, init);
 	}
 
 	SegTree(const vector<int> &in){
-		n=1;
-		while(n<in.size())n<<=1;
-		seg=vector<int>(2*n,init);
-		for(int i=n-1+in.size()-1;i>=0;i--){
-			if(n-1<=i)seg[i]=in[i-(n-1)];
-			else seg[i]=combine(seg[i*2+1],seg[i*2+2]);
+		n = 1;
+		while(n < in.size()) n <<= 1;
+		seg = vector<int>(2*n, init);
+		for(int i = n-1 + in.size() - 1; i >= 0; i--){
+			if(n-1 <= i)seg[i] = in[i-(n-1)];
+			else seg[i] = combine(seg[i*2+1], seg[i*2+2]);
 		}
 	}
 
 
-    //k-th(0-indexd) を a
-    void update(int k,int a){
-      k+=n-1;
-      seg[k]=a;
-      while(k>0){
-        k=(k-1)/2;
-        seg[k]=combine(seg[k*2+1],seg[k*2+2]);
+    //k-th(0-indexd) を a にする
+    void update(int k, int a){
+      k += n-1;
+      seg[k] = a;
+      while(k > 0){
+        k = (k-1)/2;
+        seg[k] = combine(seg[k*2+1], seg[k*2+2]);
+      }
+    }
+
+    //k-th(0-indexd) を +a する
+    void add(int k, int a){
+      k += n-1;
+      seg[k] += a;
+      while(k > 0){
+        k = (k-1)/2;
+        seg[k] = combine(seg[k*2+1], seg[k*2+2]);
       }
     }
 
     //[a,b)について調べる
-    int query(int a,int b,int k=0,int l=0,int r=-1){
-        if(r==-1)r=n;
-        if(r<=a || b<=l)return init;
-        if(a<=l &&r<=b)return seg[k];
+    int query(int a, int b, int k = 0, int l = 0, int r = -1){
+        if(r == -1) r = n;
+        if(r <= a || b <= l)return init;
+        if(a <= l && r <= b)return seg[k];
 
-        int vl=query(a,b,k*2+1,l,(l+r)/2);
-        int vr=query(a,b,k*2+2,(l+r)/2,r);
+        int vl = query(a, b, k*2+1, l, (l+r)/2);
+        int vr = query(a, b, k*2+2, (l+r)/2, r);
 
-        return combine(vl,vr);
+        return combine(vl, vr);
     }
 };
 
