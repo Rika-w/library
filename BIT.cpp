1887c41710ee3af8c5309a33dba5f08fed9e4de1
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-06-25 22:07:32 +0900

    fix BIT

---
+++ BIT.cpp
@@ -0,0 +1,33 @@
+//ant book P161
+//O(logN)
+
+//1-indexedであることに注意
+class BIT{
+private:
+    //[1,n]
+    vector<int> bit,
+    int n;
+
+public:
+    BIT(){}
+    BIT(int size){
+        n = size;
+        bit = vector<int>(n+1);
+    }
+    //0からiまでの総和
+    int sum(int i){
+        int s = 0;
+        while(i > 0){
+            s += bit[i];
+            i -= i & -i;
+        }
+        return s;
+    }
+
+    void add(int i, int x){
+        while(i <= n){
+            bit[i] += x;
+            i += i & -i;
+        }
+    }
+}
