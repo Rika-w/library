9c1d64d57a3e5c4ae2e5cd98eeed178d0b96290a
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-09-09 16:41:53 +0900

    fix SuperSet

--- SuperSet.cpp
+++ SuperSet.cpp
@@ -3,7 +3,6 @@
 #include<set>
 using namespace std;
 #define rep(i,n) for(int i = 0; i < (n); i++)
-
 class SuperSet{
 private:
 
@@ -50,21 +49,21 @@ public:
         n = size;
         bit = BIT(n);
     }
-    void add(int x){//[0,n)
+    void insert(int x){//[0,n)
         st.insert(x);
         bit.add(x,1);
     }
-    int erase(int ind){//[0,st.size())
+    int erase(int ind){//[1,st.size()]
         if(ind > bit.sum(n))return -1;
         int l = 0, r = n+1;
         while(r - l > 1){
             int mid = (l + r)/2;
-            if(ind >= bit.sum(mid))l = mid;
+            if(ind > bit.sum(mid))l = mid;
             else r = mid;
         }
-        bit.add(l,-1);
-        st.erase(l);
-        return l;
+        bit.add(l+1,-1);
+        st.erase(l+1);
+        return l+1;
     }
     int size(){
         return st.size();
@@ -78,7 +77,7 @@ public:
 
     /*
     [1,N]の値を管理するset
-    - add(x)
+    - insert(x)
         setにxを追加する
         O(logN)
     - erase(x)
@@ -89,17 +88,17 @@ public:
 
 };
 
-int main(){
-    int n = 5;
+//verify:https://atcoder.jp/contests/arc033/submissions/7433670
+
+signed main(void) {
+    int n = 200005;
     SuperSet st(n);
-    st.add(1);
-    st.add(2);
-    st.add(3);
-    st.add(4);
-    st.add(5);
-    st.show();
-    st.erase(1);
-    st.show();
-    st.erase(3);
-    st.show();
+    int q;
+    cin >> q;
+    rep(i,q){
+        int t,x;
+        cin >> t >> x;
+        if(t == 1)st.insert(x);
+        else cout << st.erase(x) << endl;
+    }
 }
